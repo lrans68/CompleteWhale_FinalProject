a7503f23721c4b5b29527ec417fba61eb999bcb9
Ryan Dew <dewrm@mail.uc.edu>
2024-11-28 20:30:59 -0500

    Added main.py and committed other changes

---
+++ CompleteWhale_FinalProject/mainPackage/main.py
@@ -0,0 +1,15 @@
+# main.py 
+
+# Name: Ryan Dew, Luke Ransick, Connor MacFarland, Anthony Caggiano
+# email: dewrm@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date:  12/10/2024
+# Course #/Section: IS4010-001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment: 
+
+# Brief Description of what this module does.
+# Citations: 
+# Anything else that's relevant: None
+
+
